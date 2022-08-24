4e2bc7cf9c4a400e1cbf8261a834d200309d6629
jeevasundar <86206633+jeevasundar@users.noreply.github.com>
2022-08-24 17:43:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function setup(){
+    canvas=createCanvas(640,480);
+    canvas.position(150,250);
+    video=createCapture(VIDEO);
+    video.hide();
+    tint_color="";}
+ 
+
+    function draw(){
+        image(video,0,0,640,480);
+        tint(tint_color);
+    }
